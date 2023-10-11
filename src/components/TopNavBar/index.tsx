0a9b28533c0df6baebff6038e5ba5f71ddbf8732
JaydenLee1116 <vv55adss@gmail.com>
2023-10-11 19:26:28 +0900

    ✨ feat #3: TopNavBar 컴포넌트 process 타입 추가

--- src/components/TopNavBar/index.tsx
+++ src/components/TopNavBar/index.tsx
@@ -1,11 +1,35 @@
+import Link from 'next/link';
+
 import Logo from '@/components/Logo';
 import ThemeSwitcher from '@/components/ThemeSwitcher';
+import { IoChevronBack, IoClose } from 'react-icons/io5';
 
-export default function TopNavBar() {
+type Props = {
+  type: 'home' | 'process';
+  prevPage?: string;
+};
+
+export default function TopNavBar({ type, prevPage = '/' }: Props) {
+  const isHome = type === 'home';
+  const isProcess = type === 'process';
   return (
-    <header className="flex h-16 w-full flex-row justify-between p-4">
-      <Logo />
-      <ThemeSwitcher />
+    <header className="flex h-16 w-full flex-row items-center justify-between p-4">
+      {isHome && (
+        <>
+          <Logo />
+          <ThemeSwitcher />
+        </>
+      )}
+      {isProcess && (
+        <>
+          <Link href={prevPage}>
+            <IoChevronBack size={24} className="text-gray_05-light dark:text-gray_05-dark" />
+          </Link>
+          <Link href="/">
+            <IoClose size={24} className="text-gray_05-light dark:text-gray_05-dark" />
+          </Link>
+        </>
+      )}
     </header>
   );
 }
