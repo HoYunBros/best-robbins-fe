92fbee2c0101570e0dfaf49ea52c360853be9343
JaydenLee1116 <vv55adss@gmail.com>
2023-10-12 01:51:06 +0900

    ✨ feat #3: Carousel 컴포넌트 네비게이션 추가

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,10 +2,12 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, A11y } from 'swiper/modules';
+import { Autoplay, A11y, Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/a11y';
 import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/keyboard';
 
 import RecipeCard from '../RecipeCard';
 import { SWIPER, STANDARD } from '@/constants';
@@ -22,6 +24,7 @@ export default function Carousel({ recommendations }: Props) {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [clickedSlideIndex, setClickedSlideIndex] = useState(-1);
   const [bodyWidth, setBodyWidth] = useState(0);
+  const [isDesktop, setIsDesktop] = useState(false);
   const handleSlideClick = (index: number) => {
     if (currentSlideIndex !== index) {
       return;
@@ -37,21 +40,26 @@ export default function Carousel({ recommendations }: Props) {
     // NOTE: mobile 유무 확인 후 390 px을 기준으로 슬라이드 거리 조정
     const bodyWidth = isMobile() ? document.body.clientWidth : STANDARD.WIDTH;
     setBodyWidth(bodyWidth);
+    setIsDesktop(!isMobile());
   }, []);
 
   return (
     <Swiper
       className="h-full w-full"
-      modules={[Autoplay, A11y]}
+      modules={[Autoplay, A11y, Navigation, Keyboard]}
       autoplay={{ delay: SWIPER.DELAY, disableOnInteraction: false }}
+      freeMode={true}
+      navigation={isDesktop}
+      keyboard={{ enabled: true }}
+      lazyPreloadPrevNext={SWIPER.LAZY_LOADED_PREV_NEXT}
       spaceBetween={bodyWidth < STANDARD.WIDTH ? bodyWidth : SWIPER.SPACE_BETWEEN}
       slidesPerView={SWIPER.SLIDES_PER_VIEW}
       loop={true}
       loopedSlides={SWIPER.LOOPED_SLIDES}
+      loopPreventsSliding={true}
       onRealIndexChange={swiper => {
         setCurrentSlideIndex(swiper.realIndex);
       }}
-      speed={SWIPER.SPEED}
       cssMode={true}
     >
       {recommendations.map((recommendation, index) => (
