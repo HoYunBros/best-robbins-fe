310f043c03c0dfbccd32842fade321db7bab6436
JaydenLee1116 <vv55adss@gmail.com>
2023-10-11 22:32:16 +0900

    ✨ feat #3: TopNavBar, FooterButton 기능 확장
    - 다른 페이지 적용 확장성 고려
    - 유저의 선택 전역상태에 따라 UI 변경 적용

--- src/components/TopNavBar/index.tsx
+++ src/components/TopNavBar/index.tsx
@@ -3,32 +3,46 @@ import Link from 'next/link';
 import Logo from '@/components/Logo';
 import ThemeSwitcher from '@/components/ThemeSwitcher';
 import { IoChevronBack, IoClose } from 'react-icons/io5';
+import BackButton from '../BackButton';
+import CloseButton from '../CloseButton';
 
 type Props = {
-  type: 'home' | 'process';
+  pageName?: 'home' | 'size' | 'ingredient' | 'result';
   prevPage?: string;
 };
 
-export default function TopNavBar({ type, prevPage = '/' }: Props) {
-  const isHome = type === 'home';
-  const isProcess = type === 'process';
+export default function TopNavBar({ pageName = 'home', prevPage = '/' }: Props) {
+  const isHome = pageName === 'home';
+  const isProcess = pageName !== 'home';
+  const processWidth = {
+    size: 'w-[33%]',
+    ingredient: 'w-[67%]',
+    result: 'w-full',
+  };
   return (
-    <header className="flex h-16 w-full flex-row items-center justify-between p-4">
-      {isHome && (
-        <>
-          <Logo />
-          <ThemeSwitcher />
-        </>
-      )}
+    <header className="relative flex h-16 w-full flex-col items-center justify-between">
+      <div
+        className={`flex h-16 w-full items-center justify-between ${
+          isProcess && 'border-b-2'
+        } border-gray_02-light p-4 dark:border-gray_02-dark`}
+      >
+        {isHome && (
+          <>
+            <Logo />
+            <ThemeSwitcher />
+          </>
+        )}
+        {isProcess && (
+          <>
+            <BackButton prevPage={prevPage} />
+            <CloseButton />
+          </>
+        )}
+      </div>
       {isProcess && (
-        <>
-          <Link href={prevPage}>
-            <IoChevronBack size={24} className="text-gray_05-light dark:text-gray_05-dark" />
-          </Link>
-          <Link href="/">
-            <IoClose size={24} className="text-gray_05-light dark:text-gray_05-dark" />
-          </Link>
-        </>
+        <div
+          className={`absolute bottom-0 h-[2px] bg-gray_05-light dark:bg-gray_05-dark ${processWidth[pageName]} left-0`}
+        ></div>
       )}
     </header>
   );
