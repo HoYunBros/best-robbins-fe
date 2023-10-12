683ce17133fa7b6c8ffa7ac754f84cd15caf933a
JaydenLee1116 <vv55adss@gmail.com>
2023-10-12 20:07:55 +0900

    ✨ feat #13: Result 페이지 컴포넌트 구현
    - param을 받아 해당 id의 값으로 get 요청

---
+++ src/app/(combination)/result/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { notFound } from 'next/navigation';
+
+import TopNavBar from '@/components/common/TopNavBar';
+import RecipeCard from '@/components/common/RecipeCard';
+import ResultFooter from '@/components/result/ResultFooter';
+import { getRecipe } from '@/services/getRecipe';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function ResultPage({ params }: Props) {
+  const recipeId = parseInt(params.slug, 10);
+
+  if (Number.isNaN(recipeId)) {
+    notFound();
+  }
+
+  const result = await getRecipe(recipeId);
+  return (
+    <>
+      <TopNavBar pageName="result" prevPage="/ingredient" />
+      <main className="flex h-full w-full flex-1 flex-col justify-start gap-y-5 px-5 pt-[34px]">
+        <h2 className="text-x_large font-semi_bold">나만의 꿀조합을 찾았어요!</h2>
+        <div
+          className={`relative flex h-[460px] w-[340px] items-center justify-center rounded-3xl shadow-light transition-all dark:shadow-dark`}
+        >
+          <RecipeCard recipe={result} hasBookmark={true} />
+        </div>
+      </main>
+      <ResultFooter nextPage="/size" />
+    </>
+  );
+}
