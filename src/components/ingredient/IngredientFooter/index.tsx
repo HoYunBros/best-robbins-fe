8a755a0b1caa7d7943ad9fe451fe6c6ed3a04706
JaydenLee1116 <vv55adss@gmail.com>
2023-10-12 20:04:16 +0900

    ✨ feat #11: IngredientFooter 라우팅 수정
    - 동적으로 id를 받아서 라우팅 처리

--- src/components/ingredient/IngredientFooter/index.tsx
+++ src/components/ingredient/IngredientFooter/index.tsx
@@ -1,7 +1,8 @@
 'use client';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import { useUserSelectStore } from '@/stores/useUserSelectStore';
+import { postRecipe } from '@/services/postRecipe';
 
 import { Ingredient } from '@/types';
 
@@ -23,20 +24,26 @@ export default function IngredientFooter({ nextPage, ingredients }: Props) {
   const isSelected = userSelect.ingredientIds.length !== 0;
   const disabled = !isAbleToRecommend;
 
+  const router = useRouter();
+
   return (
-    <footer className="relative flex h-20 w-full flex-col items-center justify-around border-t border-t-gray_00-light p-2 pb-8 dark:border-t-gray_00-dark">
+    <footer className="relative flex h-20 w-full items-center justify-around border-t border-t-gray_00-light p-2 pb-8 dark:border-t-gray_00-dark">
       {!isSelected || isAbleToRecommend || (
-        <p className="absolute top-[-20px] text-small font-semi_bold text-gray_05-light dark:text-gray_05-dark">
+        <p className="absolute top-[-32px] text-small font-semi_bold text-gray_05-light dark:text-gray_05-dark">
           재료를 조금 더 골라볼까요?
         </p>
       )}
-      <Link
-        href={disabled ? '#' : nextPage}
+      <button
         className={`flex h-12 w-[340px] items-center justify-center rounded-lg border-0 bg-primary-light px-6 text-medium font-semi_bold text-white-light
         ${disabled && 'cursor-not-allowed opacity-50'}`}
+        onClick={async () => {
+          if (disabled) return;
+          const { id } = await postRecipe(userSelect);
+          router.push(`${nextPage}/${id}`);
+        }}
       >
         결과보기
-      </Link>
+      </button>
     </footer>
   );
 }
